c66356b13cf66123f56789c8c375cd26021095d4
Shawdooow <banhammer1915@gmail.com>
2017-05-31 12:48:01 -0400

    Remove player Shooting

--- osu.Game.Rulesets.Vitaru/Objects/HitObjectType.cs
+++ osu.Game.Rulesets.Vitaru/Objects/HitObjectType.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
-
-namespace osu.Game.Rulesets.Vitaru.Objects
+﻿namespace osu.Game.Rulesets.Vitaru.Objects
 {
     public enum HitObjectType
     {
